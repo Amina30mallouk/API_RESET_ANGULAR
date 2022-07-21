258da9d029a1670f0175988e931bd06728205e76
amina mallouk <amina.mallouk30@gmail.com>
2022-07-21 16:52:14 +0100

    last one

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -8,6 +8,10 @@ import { ThisReceiver } from '@angular/compiler';
   styleUrls: ['./tasks.component.scss']
 })
 export class TasksComponent implements OnInit {
+
+  MyTask : Task = {label : '',
+  completed: false };
+  
 tasks: Task[] = [] ;  
   constructor(private taskService : TasksService ) { }
 
@@ -18,5 +22,46 @@ getTasks(){
 this.taskService.findAll() 
      .subscribe(tasks => this.tasks = tasks);
 }
+deleteTask(id:any){
+  this.taskService.delete(id)
+  . subscribe(()=> {
+this.tasks = this.tasks.filter(task => task.id != id)
+  })
+}
 
+persistTask(){
+  this.taskService.persist(this.MyTask)
+  .subscribe((task)=>{
+    this.tasks = [task, ...this.tasks]
+  })
+  this.resetTask();
+}
+
+resetTask(){
+  this.MyTask = {
+    label :'',
+    completed: false
+  }
+   
+}
+// completedTask(task:Task){
+//   this.taskService.completed(task.id,task.completed)
+//        .subscribe(()=> {
+//     task.completed = !task.completed
+    
+//   })
+// }
+
+editTask(task:Task){
+  this.MyTask = task ;
+
+}
+updatetask(){
+  this.taskService.updateTask(this.MyTask)
+      .subscribe((task) => {
+        this.resetTask();
+        
+      })
+}
+ 
 } 
--- src/app/services/tasks.service.ts
+++ src/app/services/tasks.service.ts
@@ -1,16 +1,31 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import  { Task } from 'src/app/models/task';
 @Injectable({
   providedIn: 'root'
 })
 export class TasksService {
-
+apiURL = "http://localhost:3000/tasks";
   constructor(private http: HttpClient) { }
 
  findAll(){
 
-return this.http.get<Task[]>("http://localhost:3000/tasks"); 
+return this.http.get<Task[]>(this.apiURL); 
+ }
+
+ delete(id: any){
+  return this.http.delete('${this.apiURL}/${id}');
+ }
+
+ persist(task:any){
+  return this.http.post<Task>(this.apiURL, task);
+ }
+completed(id:number,completed:boolean){
+  this.http.patch('${this.apiURL}/${id}',{completed:!completed})
+
+}
+ updateTask(task:Task){
+  return this.http.put('${this.apiURL}/${task.id}', task);
  }
 
 }
